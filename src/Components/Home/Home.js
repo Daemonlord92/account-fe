c5d1b173ff92e650a40e911b90aa7290fb01b42a
Matthew Martin <zhintateo19924559@gmail.com>
2021-01-14 12:44:40 -0600

    created a few Boilerplate starts for a couple pages LOGIN, BALANCESHEET AND NAV

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<>
+		
+			<div class="row">
+				<div class="col-md-12">
+					<div class="jumbotron">
+						<h2>
+							Welcome To the Generic Accounting Software!
+						</h2>
+						<p>
+							The Goal with this software is to Simplfiy your accounting needs. Updates will occur pretty often to meet standards!
+						</p>
+						<p>
+							<a class="btn btn-primary btn-large" href="#">Login</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		
+		</>
+	)
+}
+
+export default Home;
